b9c202359e72b743528c2e87a82498b15989698a
Junior Santos <juniorsantosdvlp@gmail.com>
2021-07-10 20:16:32 -0300

    Atualizacao de Patch e update

--- src/main/java/br/com/kanritech/market/controller/ProductController.java
+++ src/main/java/br/com/kanritech/market/controller/ProductController.java
@@ -2,15 +2,17 @@ package br.com.kanritech.market.controller;
 
 import br.com.kanritech.market.model.Product;
 import br.com.kanritech.market.repository.ProductRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
 @RestController
-@RequestMapping("product")
+@RequestMapping("products")
 @CrossOrigin
 public class ProductController {
 
@@ -23,14 +25,40 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public Optional<Product> findProduct(@PathVariable("id") Long id) {
-
-
-        return productRepository.findById(id);
+    public Product findProduct(@PathVariable("id") Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping
     public Iterable<Product> findAll() {
         return productRepository.findAll();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> fullUpdateProduct(@PathVariable("id") Long id, @RequestBody Product requestProduct) {
+        Product foundProduct = findProduct(id);
+        foundProduct = requestProduct;
+        foundProduct.setId(id);
+        return ResponseEntity.ok(productRepository.save(foundProduct));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Product> patchUpdateProduct(@PathVariable("id") Long id, @RequestBody Product requestProduct) {
+        Product foundProduct = findProduct(id);
+        foundProduct.setCode(Optional.ofNullable(requestProduct.getCode()).orElse(foundProduct.getCode()));
+        foundProduct.setDepartment(Optional.ofNullable(requestProduct.getDepartment()).orElse(foundProduct.getDepartment()));
+        foundProduct.setDescription(Optional.ofNullable(requestProduct.getDescription()).orElse(foundProduct.getDescription()));
+        foundProduct.setName(Optional.ofNullable(requestProduct.getName()).orElse(foundProduct.getName()));
+        foundProduct.setSellValue(Optional.ofNullable(requestProduct.getSellValue()).orElse(foundProduct.getSellValue()));
+        return ResponseEntity.ok().body(productRepository.save(foundProduct));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id){
+        Product product = findProduct(id);
+        productRepository.delete(product);
+        return ResponseEntity.ok().body(product);
+
+    }
 }
+
+
